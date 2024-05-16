46657cc3c1c55246181efc5a1e28ee09678014c2
Alexey <naumchik199@mail.ru>
2024-05-16 05:58:04 +0300

    Python logistic-regression

---
+++ logistic-regression.py
@@ -0,0 +1,73 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class LogisticRegression:
+    def __init__(self, learning_rate=0.01, num_iterations=100000, fit_intercept=True, verbose=False):
+        # initializing logistic regression model parameters
+        self.learning_rate = learning_rate
+        self.num_iterations = num_iterations
+        self.fit_intercept = fit_intercept
+        self.verbose = verbose
+
+    def __add_intercept(self, X):
+        intercept = np.ones((X.shape[0], 1))
+        return np.concatenate((intercept, X), axis=1)
+
+    def __sigmoid(self, z):
+        return 1 / (1 + np.exp(-z))
+
+    def __loss(self, h, y):
+        # logistic regression loss function (L)
+        return (-y * np.log(h) - (1 - y) * np.log(1 - h)).mean()
+
+    def fit(self, X, y):
+        if self.fit_intercept:
+            X = self.__add_intercept(X)
+        # weights initialization
+        self.theta = np.zeros(X.shape[1])
+        # gradient descent for fitting the weights
+        for i in range(self.num_iterations):
+            z = np.dot(X, self.theta)
+            h = self.__sigmoid(z)
+            gradient = np.dot(X.T, (h - y)) / y.size
+            self.theta -= self.learning_rate * gradient
+            if (self.verbose == True and i % 10000 == 0):
+                z = np.dot(X, self.theta)
+                h = self.__sigmoid(z)
+                print(f'Loss: {self.__loss(h, y)}')
+
+    def predict_prob(self, X):
+        if self.fit_intercept:
+            X = self.__add_intercept(X)
+        # return sigmoid function for X values and learned theta
+        return self.__sigmoid(np.dot(X, self.theta))
+
+    def predict(self, X, threshold=0.5):
+        return self.predict_prob(X) >= threshold
+
+# Generate sample data with standard deviation
+np.random.seed(42)
+X1 = np.random.randn(100, 2) * 1.4 + [4, 4]
+X2 = np.random.randn(100, 2) * 1.4 + [-4, -4]
+X = np.vstack([X1, X2])
+y = np.hstack([np.zeros(100), np.ones(100)])
+
+# Create an instance of Logistic Regression Classifier
+model = LogisticRegression(learning_rate=0.1, num_iterations=300000, verbose=True)
+
+# Fit the model to the data
+model.fit(X, y)
+
+# Predict probabilities
+probabilities = model.predict_prob(X)
+
+# Plot the decision boundary
+plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.coolwarm, s=100)
+x1_min, x1_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+x2_min, x2_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+xx1, xx2 = np.meshgrid(np.linspace(x1_min, x1_max), np.linspace(x2_min, x2_max))
+grid = np.c_[xx1.ravel(), xx2.ravel()]
+probs = model.predict_prob(grid).reshape(xx1.shape)
+plt.contour(xx1, xx2, probs, [0.5], linewidths=1, colors='black')
+
+plt.show()
